a5424aa990d4178802af937a660d62f558c9614b
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-09-02 14:28:31 -0300

    adicionando o credo

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,12 @@
 defmodule ExMon do
+  
+    @moduledoc """
+    Modulo principal do programa
+    """
+
   alias ExMon.Player
 
+  
   def create_palyer(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
--- lib/player.ex
+++ lib/player.ex
@@ -1,11 +1,21 @@
 defmodule ExMon.Player do
+    @moduledoc """
+    Modulo para criar a struct do player
+    """
+
+
+    @doc """
+    Variaveis de modulos para criar a struct
+    """
     @required_keys  [:life, :name, :move_rnd, :move_avg, :move_heal] 
     @life 100
-    
+
     @enforce_keys @required_keys
     defstruct @required_keys
 
-
+    @doc """
+    Criação da struct
+    """
     def build(name, move_rnd, move_avg, move_heal) do
         %ExMon.Player {
             name: name, 
