665d8b0cf1fec51e5810bd5b64a97abdb1d1c980
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-09-04 14:20:53 -0300

    terminando estado do start game

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,18 +1,19 @@
 defmodule ExMon do
-  
+
     @moduledoc """
     Modulo principal do programa
     """
 
   alias ExMon.Player
   alias ExMon.Game
-  
+
   def create_palyer(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
   end
 
   def start_game(player) do
-    computer = create_palyer("robo", :soco, :chute, :cura)
-    Game.start(computer, player)    
+    "robo"
+    |> create_palyer(:soco, :chute, :cura)
+    |> Game.start(player)
   end
 end
