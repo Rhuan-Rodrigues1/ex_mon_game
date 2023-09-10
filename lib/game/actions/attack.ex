f0c3e74c4680a639aa299bdf3605b4d786b9f1dc
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-09-10 14:14:26 -0300

    update state attack

--- lib/game/actions/attack.ex
+++ lib/game/actions/attack.ex
@@ -1,6 +1,7 @@
 defmodule ExMon.Game.Actions.Attack do
 
     alias ExMon.Game
+    alias ExMon.Game.Status
 
     @move_avg_range  10..25
     @move_rnd_range 10..35
@@ -12,17 +13,26 @@ defmodule ExMon.Game.Actions.Attack do
         |> Game.fetch_player()
         |> Map.get(:life)
         |> calculate_total_life(demage)
-        |> update_life(oponent)
+        |> update_life(oponent, demage)
 
     end
 
+    
     defp generate_demage(:move_avg), do: Enum.random(@move_avg_range)
     defp generate_demage(:move_rnd), do: Enum.random(@move_rnd_range)
     defp calculate_total_life(life, demage) when life - demage < 0, do: 0
     defp calculate_total_life(life, demage), do: life - demage
-    defp update_life(life, oponent) do
+    defp update_life(life, oponent, demage) do
         oponent
         |> Game.fetch_player()
         |> Map.put(:life, life)
+        |> update_state(oponent, demage)
+    end
+    def update_state(player, oponent, demage) do
+        Game.info()
+        |> Map.put(oponent, player)
+        |> Game.update()
+
+        Status.print_demage_receive(oponent, :attack, demage)
     end
 end
--- lib/game/status.ex
+++ lib/game/status.ex
@@ -9,4 +9,8 @@ defmodule ExMon.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts "\n======= Movimento inválido: #{move} =======\n"
   end
+  
+  def print_demage_receive(:computer, :attack, demage) do
+    IO.puts "\n======= Player atacou: #{demage} de dano =======\n"
+  end
 end
